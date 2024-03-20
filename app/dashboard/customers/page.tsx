e05a4cab98dc47e55785bdab67f7817c01a4de4f
D-Fluster <158021342+D-Fluster@users.noreply.github.com>
2024-03-20 10:11:08 -0700

    Post-tutorial; partial customer list (no invoice data or searchability)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,18 +1,20 @@
 import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import CustomersTable from '@/app/ui/customers/table';
-import customersList from '@/app/lib/placeholder-data';
+import { customers as customersList } from '@/app/lib/placeholder-data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function CustomersPage() {
+  console.log(customersList);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         {/* <h1 className={`${lusitana.className} text-2xl`}>Customers</h1> */}
-        <CustomersTable customers={[customersList]} />
+        <CustomersTable customers={customersList} />
       </div>
     </div>
   );
